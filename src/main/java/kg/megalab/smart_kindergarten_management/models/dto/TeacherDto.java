520bb1382940bd29b457c2e8071a232a9c95e44d
SuNoora <snurzadas02@gmail.com>
2025-10-01 00:48:56 +0600

    Add annotations @Data, @FieldDefaults(level = AccessLevel.PRIVATE),@AllArgsConstructor, @NoArgsConstructor

--- src/main/java/kg/megalab/smart_kindergarten_management/models/dto/TeacherDto.java
+++ src/main/java/kg/megalab/smart_kindergarten_management/models/dto/TeacherDto.java
@@ -3,7 +3,16 @@ package kg.megalab.smart_kindergarten_management.models.dto;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import kg.megalab.smart_kindergarten_management.models.enums.TeacherDegree;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
 
+@Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@AllArgsConstructor
+@NoArgsConstructor
 public class TeacherDto {
 
     Long id;
