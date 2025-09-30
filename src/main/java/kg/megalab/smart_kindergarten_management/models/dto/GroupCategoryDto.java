6185b68dab271fa05f4bbade1717e5ebda94d5ba
SuNoora <snurzadas02@gmail.com>
2025-09-30 23:26:48 +0600

    Change "Integer" to "Double" and add annotations @AllArgsConstructor,@NoArgsConstructor

--- src/main/java/kg/megalab/smart_kindergarten_management/models/dto/GroupCategoryDto.java
+++ src/main/java/kg/megalab/smart_kindergarten_management/models/dto/GroupCategoryDto.java
@@ -4,21 +4,24 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
 
 @Data
 @FieldDefaults(level = AccessLevel.PRIVATE)
+@AllArgsConstructor
+@NoArgsConstructor
 public class GroupCategoryDto {
-    @Positive
-    Long id;
 
     @NotBlank(message = "Название категории обязательно")
     String name;
+
     @NotNull(message = "Статус активности обязателен")
     Boolean active;
 
     @NotNull(message = "Цена обязательна")
     @Positive(message = "Цена должна быть положительной")
-    Integer price;
+    Double price; // Унифицированный тип
 }
